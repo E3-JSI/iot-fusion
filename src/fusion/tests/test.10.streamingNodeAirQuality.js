5af85bb3db5adc55139f678607804a373f8a4af1
Klemen Kenda <klemen.kenda@ijs.si>
2020-08-10 15:04:34 +0200

    Unit tests for combined air quality node

--- src/fusion/tests/test.10.streamingNodeAirQuality.js
+++ src/fusion/tests/test.10.streamingNodeAirQuality.js
@@ -148,7 +148,7 @@ let fusionConfig = {
             "attributes": [
                 { "time": 0, "attributes": [                                           // current time
                     { type: "value", "name": "pm100" },
-                    { type: "value", "name": "t|ma|21600" },
+                    { type: "value", "name": "temp|ma|21600" },
                     { type: "value", "name": "no2|ma|86400000" },
                     { type: "value", "name": "o3|min|86400000" },
                     { type: "value", "name": "pm025|max|86400000" },
@@ -159,7 +159,7 @@ let fusionConfig = {
                 ]},
                 { "time": -24 * 60 * 60 * 1000, "attributes": [                        // 24h ago
                     { type: "value", "name": "pm100" },
-                    { type: "value", "name": "t|ma|21600" },
+                    { type: "value", "name": "temp|ma|21600" },
                     { type: "value", "name": "no2|ma|86400000" },
                     { type: "value", "name": "o3|min|86400000" },
                     { type: "value", "name": "pm025|max|86400000" },
@@ -211,16 +211,21 @@ describe('streamingAirQualityNode', function() {
             assert.deepEqual(saqn.rawstore.fields, [
                 { id: 0, name: 'Time', type: 'datetime', nullable: false, internal: false, primary: false },
                 { id: 1, name: 'rh', type: 'float', nullable: false, internal: false, primary: false },
-                { id: 2, name: 't', type: 'float', nullable: false, internal: false, primary: false },
+                { id: 2, name: 'temp', type: 'float', nullable: false, internal: false, primary: false },
                 { id: 3, name: 'no2', type: 'float', nullable: false, internal: false, primary: false },
                 { id: 4, name: 'o3', type: 'float', nullable: false, internal: false, primary: false },
                 { id: 5, name: 'pm025', type: 'float', nullable: false, internal: false, primary: false },
-                { id: 6, name: 'pm100', type: 'float', nullable: false, internal: false, primary: false }
+                { id: 6, name: 'pm100', type: 'float', nullable: false, internal: false, primary: false },
+                { id: 7, name: 'carno', type: 'float', nullable: false, internal: false, primary: false },
+                { id: 8, name: 'vavg', type: 'float', nullable: false, internal: false, primary: false },
+                { id: 9, name: 'vmax', type: 'float', nullable: false, internal: false, primary: false },
+                { id: 10, name: 'vmin', type: 'float', nullable: false, internal: false, primary: false },
+                { id: 11, name: 'w', type: 'float', nullable: false, internal: false, primary: false },
             ]);
         });
 
         it('aggregates initialized - number', function() {
-            assert.equal(Object.keys(saqn.aggregate).length, 50);
+            assert.equal(Object.keys(saqn.aggregate).length, 80);
         });
 
         it('aggregates initialized - key names', function() {
@@ -238,19 +243,19 @@ describe('streamingAirQualityNode', function() {
                 "rh|variance|86400000",
                 "rh|winbuf|604800000",
                 "rh|ma|604800000",
-                "t|tick",
-                "t|winbuf|21600000",
-                "t|ma|21600000",
-                "t|min|21600000",
-                "t|max|21600000",
-                "t|variance|21600000",
-                "t|winbuf|86400000",
-                "t|ma|86400000",
-                "t|min|86400000",
-                "t|max|86400000",
-                "t|variance|86400000",
-                "t|winbuf|604800000",
-                "t|ma|604800000",
+                "temp|tick",
+                "temp|winbuf|21600000",
+                "temp|ma|21600000",
+                "temp|min|21600000",
+                "temp|max|21600000",
+                "temp|variance|21600000",
+                "temp|winbuf|86400000",
+                "temp|ma|86400000",
+                "temp|min|86400000",
+                "temp|max|86400000",
+                "temp|variance|86400000",
+                "temp|winbuf|604800000",
+                "temp|ma|604800000",
                 "no2|tick",
                 "no2|winbuf|21600000",
                 "no2|ma|21600000",
@@ -275,6 +280,36 @@ describe('streamingAirQualityNode', function() {
                 "pm100|min|21600000",
                 "pm100|max|21600000",
                 "pm100|variance|21600000",
+                "carno|tick",
+                "carno|winbuf|21600000",
+                "carno|ma|21600000",
+                "carno|min|21600000",
+                "carno|max|21600000",
+                "carno|variance|21600000",
+                "vavg|tick",
+                "vavg|winbuf|21600000",
+                "vavg|ma|21600000",
+                "vavg|min|21600000",
+                "vavg|max|21600000",
+                "vavg|variance|21600000",
+                "vmax|tick",
+                "vmax|winbuf|21600000",
+                "vmax|ma|21600000",
+                "vmax|min|21600000",
+                "vmax|max|21600000",
+                "vmax|variance|21600000",
+                "vmin|tick",
+                "vmin|winbuf|21600000",
+                "vmin|ma|21600000",
+                "vmin|min|21600000",
+                "vmin|max|21600000",
+                "vmin|variance|21600000",
+                "w|tick",
+                "w|winbuf|21600000",
+                "w|ma|21600000",
+                "w|min|21600000",
+                "w|max|21600000",
+                "w|variance|21600000",
             ]);
         });
 
@@ -371,18 +406,28 @@ describe('streamingAirQualityNode', function() {
     describe('data insertion', function() {
 
         it ('data record saved correctly', function() {
-            saqn.processRecord(JSON.parse('{"stampm": 1468493071000, "rh": 0.0, "t": 1.0, "no2": 2.0, "o3": 3.0, "pm025": 4.0, "pm100": 5.0 }'));
-            saqn.processRecord(JSON.parse('{"stampm": 1468493072000, "rh": 1.0, "t": 2.0, "no2": 3.0, "o3": 4.0, "pm025": 5.0, "pm100": 6.0 }'));
+            saqn.processRecord(JSON.parse('{"stampm": 1468493071000, "rh": 0.0, "temp": 1.0, "no2": 2.0, "o3": 3.0, "pm025": 4.0, "pm100": 5.0, "carno": 6, "vavg": 7, "vmax": 8, "vmin": 6, "w": 9 }'));
+            saqn.processRecord(JSON.parse('{"stampm": 1468493072000, "rh": 1.0, "temp": 2.0, "no2": 3.0, "o3": 4.0, "pm025": 5.0, "pm100": 6.0, "carno": 7, "vavg": 8, "vmax": 9, "vmin": 7, "w": 10 }'));
             assert.equal(saqn.buffer.length, 2);
             assert.equal(saqn.buffer[0].pm100, 5.0);
             assert.equal(saqn.buffer[1].pm100, 6.0);
             assert.equal(saqn.buffer[1].rh, 1);
+            assert.equal(saqn.buffer[1].carno, 7);
+            assert.equal(saqn.buffer[1].vavg, 8);
+            assert.equal(saqn.buffer[1].vmax, 9);
+            assert.equal(saqn.buffer[1].vmin, 7);
+            assert.equal(saqn.buffer[1].w, 10);
         });
 
         it ('Empty values in message set to 0', function() {
             saqn.processRecord(JSON.parse('{"stampm": 1468493073000}'));
             assert.equal(saqn.buffer.length, 3);
             assert.equal(saqn.buffer[2].pm100, 0);
+            assert.equal(saqn.buffer[2].carno, 0);
+            assert.equal(saqn.buffer[2].vavg, 0);
+            assert.equal(saqn.buffer[2].vmax, 0);
+            assert.equal(saqn.buffer[2].vmin, 0);
+            assert.equal(saqn.buffer[2].w, 0);
         });
 
         it ('stream aggregates calculated correctly for #2 insertion', function() {
@@ -418,16 +463,41 @@ describe('streamingAirQualityNode', function() {
                 "rh|min|86400000": 0,
                 "rh|variance|21600000": 0.5,
                 "rh|variance|86400000": 0.5,
-                "t": 2,
-                "t|max|21600000": 2,
-                "t|max|86400000": 2,
-                "t|ma|21600000": 1.5,
-                "t|ma|604800000": 1.5,
-                "t|ma|86400000": 1.5,
-                "t|min|21600000": 1,
-                "t|min|86400000": 1,
-                "t|variance|21600000": 0.5,
-                "t|variance|86400000": 0.5
+                "temp": 2,
+                "temp|max|21600000": 2,
+                "temp|max|86400000": 2,
+                "temp|ma|21600000": 1.5,
+                "temp|ma|604800000": 1.5,
+                "temp|ma|86400000": 1.5,
+                "temp|min|21600000": 1,
+                "temp|min|86400000": 1,
+                "temp|variance|21600000": 0.5,
+                "temp|variance|86400000": 0.5,
+                "carno": 7,
+                "carno|max|21600000": 7,
+                "carno|ma|21600000": 6.5,
+                "carno|min|21600000": 6,
+                "carno|variance|21600000": 0.5,
+                "vavg": 8,
+                "vavg|max|21600000": 8,
+                "vavg|ma|21600000": 7.5,
+                "vavg|min|21600000": 7,
+                "vavg|variance|21600000": 0.5,
+                "vmax": 9,
+                "vmax|max|21600000": 9,
+                "vmax|ma|21600000": 8.5,
+                "vmax|min|21600000": 8,
+                "vmax|variance|21600000": 0.5,
+                "vmin": 7,
+                "vmin|max|21600000": 7,
+                "vmin|ma|21600000": 6.5,
+                "vmin|min|21600000": 6,
+                "vmin|variance|21600000": 0.5,
+                "w": 10,
+                "w|max|21600000": 10,
+                "w|ma|21600000": 9.5,
+                "w|min|21600000": 9,
+                "w|variance|21600000": 0.5
             });
         });
 
@@ -440,7 +510,16 @@ describe('streamingAirQualityNode', function() {
                 let o3 = i + 4;
                 let pm025 = i + 5;
                 let pm100 = i + 6;
-                saqn.processRecord(JSON.parse('{"stampm": ' + time + ', "rh": ' + rh + ', "t": ' + t + ', "no2": ' + no2 + ', "o3": ' + o3 + ', "pm025": ' + pm025 + ', "pm100": ' + pm100 + ' }'));
+                let carno = i + 7;
+                let vavg = i + 8;
+                let vmax = i + 9;
+                let vmin = i + 7;
+                let w = i + 10;
+                saqn.processRecord(JSON.parse(
+                    '{"stampm": ' + time + ', "rh": ' + rh + ', "temp": ' + t + ', "no2": ' + no2 + ', "o3": ' + o3 +
+                    ', "pm025": ' + pm025 + ', "pm100": ' + pm100 +
+                    ', "carno": ' + carno + ', "vavg": ' + vavg + ', "vmax": ' + vmax + ', "vmin": ' + vmin + ', "w": ' + w +
+                    ' }'));
             }
             assert.equal(saqn.buffer.length, 12);
 
@@ -476,16 +555,42 @@ describe('streamingAirQualityNode', function() {
                 "rh|min|86400000": 0,
                 "rh|variance|21600000": 12.083333333333336,
                 "rh|variance|86400000": 12.083333333333336,
-                "t": 11,
-                "t|max|21600000": 11,
-                "t|max|86400000": 11,
-                "t|ma|21600000": 5.5,
-                "t|ma|604800000": 5.5,
-                "t|ma|86400000": 5.5,
-                "t|min|21600000": 0,
-                "t|min|86400000": 0,
-                "t|variance|21600000": 13,
-                "t|variance|86400000": 13
+                "temp": 11,
+                "temp|max|21600000": 11,
+                "temp|max|86400000": 11,
+                "temp|ma|21600000": 5.5,
+                "temp|ma|604800000": 5.5,
+                "temp|ma|86400000": 5.5,
+                "temp|min|21600000": 0,
+                "temp|min|86400000": 0,
+                "temp|variance|21600000": 13,
+                "temp|variance|86400000": 13,
+                "carno": 16,
+                "carno|max|21600000": 16,
+                "carno|ma|21600000": 10.083333333333332,
+                "carno|min|21600000": 0,
+                "carno|variance|21600000": 20.08333333333333,
+                "vavg": 17,
+                "vavg|max|21600000": 17,
+                "vavg|ma|21600000": 11,
+                "vavg|min|21600000": 0,
+                "vavg|variance|21600000": 22,
+                "vmax": 18,
+                "vmax|max|21600000": 18,
+                "vmax|ma|21600000": 11.916666666666666,
+                "vmax|min|21600000": 0,
+                "vmax|variance|21600000": 24.08333333333334,
+                "vmin": 16,
+                "vmin|max|21600000": 16,
+                "vmin|ma|21600000": 10.083333333333332,
+                "vmin|min|21600000": 0,
+                "vmin|variance|21600000": 20.08333333333333,
+                "w": 19,
+                "w|max|21600000": 19,
+                "w|ma|21600000": 12.833333333333332,
+                "w|min|21600000": 0,
+                "w|variance|21600000": 26.33333333333333
+
             });
         });
 
