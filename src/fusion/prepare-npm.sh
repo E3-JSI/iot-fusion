e8e0a885348d166dd9d1ba5c0ed35c70d31bd408
Klemen Kenda <klemen.kenda@ijs.si>
2020-08-10 15:10:59 +0200

    Fixing prepare NPM

--- src/fusion/prepare-npm.sh
+++ src/fusion/prepare-npm.sh
@@ -17,10 +17,16 @@ cd dist
 rm index.js
 javascript-obfuscator streamFusion.js --output streamFusion.js --self-defending true --compact true
 javascript-obfuscator streamModel.js --output streamModel.js --self-defending true --compact true
+javascript-obfuscator streamMaster.js --output streamMaster.js --self-defending true --compact true
+
 javascript-obfuscator nodes/streamingNode.js --output nodes/streamingNode.js --self-defending true --compact true
+javascript-obfuscator nodes/streamingAirQualityNode.js --output nodes/streamingAirQualityNode.js --self-defending true --compact true
 javascript-obfuscator nodes/streamingEnergyNode.js --output nodes/streamingEnergyNode.js --self-defending true --compact true
-javascript-obfuscator nodes/streamingSubstationNode.js --output nodes/streamingSubstationNode.js --self-defending true --compact true
+javascript-obfuscator nodes/streamingSmartLampNode.js --output nodes/streamingSmartLampNode.js --self-defending true --compact true
 javascript-obfuscator nodes/streamingStaticNode.js --output nodes/streamingStaticNode.js --self-defending true --compact true
+javascript-obfuscator nodes/streamingSubstationNode.js --output nodes/streamingSubstationNode.js --self-defending true --compact true
+javascript-obfuscator nodes/streamingTimeValueNode.js --output nodes/streamingTimeValueNode.js --self-defending true --compact true
+javascript-obfuscator nodes/streamingTrafficCounterNode.js --output nodes/streamingTrafficCounterNode.js --self-defending true --compact true
 javascript-obfuscator nodes/streamingTrainNode.js --output nodes/streamingTrainNode.js --self-defending true --compact true
 javascript-obfuscator nodes/streamingWeatherNode.js --output nodes/streamingWeatherNode.js --self-defending true --compact true
 rm nodes/streamingSpecialNodeTemplate.js
@@ -40,7 +46,12 @@ javascript-obfuscator tests/test.3.streamingNodeStatic.js --output tests/test.3.
 javascript-obfuscator tests/test.4.streamingNodeWeather.js --output tests/test.4.streamingNodeWeather.js --self-defending true --compact true
 javascript-obfuscator tests/test.5.streamingNodeSubstation.js --output tests/test.5.streamingNodeSubstation.js --self-defending true --compact true
 javascript-obfuscator tests/test.6.streamingNodeTrain.js --output tests/test.6.streamingNodeTrain.js --self-defending true --compact true
+javascript-obfuscator tests/test.7.streamingNodeTimeValue.js --output tests/test.6.streamingNodeTimeValue.js --self-defending true --compact true
+javascript-obfuscator tests/test.8.streamingNodeSmartLamp.js --output tests/test.6.streamingNodeSmartLamp.js --self-defending true --compact true
+javascript-obfuscator tests/test.9.streamingNodeTrafficCounter.js --output tests/test.6.streamingNodeTrafficCounter.js --self-defending true --compact true
+javascript-obfuscator tests/test.10.streamingNodeAirQuality.js --output tests/test.6.streamingNodeAirQuality.js --self-defending true --compact true
 javascript-obfuscator tests/test.90.streamFusion.js --output tests/test.90.streamFusion.js --self-defending true --compact true
+javascript-obfuscator tests/test.91.streamFusionFactory.js --output tests/test.90.streamFusionFactory.js --self-defending true --compact true
 
 
 cd ..
